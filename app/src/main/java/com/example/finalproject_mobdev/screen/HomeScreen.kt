fe4c8eba213a9756504ba67fa107915c207516ed
Luanna Virginia <luandrade.melo@gmail.com>
2024-12-04 13:38:38 +0000

    sidebarOK missing the implementaions for 2 buttons

--- app/src/main/java/com/example/finalproject_mobdev/screen/HomeScreen.kt
+++ app/src/main/java/com/example/finalproject_mobdev/screen/HomeScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -26,12 +27,15 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.launch
 import java.util.Locale
+import androidx.compose.foundation.background
 
-@OptIn(ExperimentalPermissionsApi::class)
+
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     onLogout: () -> Unit,
     onNavigateToPubDetails: (String) -> Unit // Pass pubId to navigate to PubDetailsScreen
+
 ) {
     val context = LocalContext.current
     val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
@@ -44,6 +48,9 @@ fun HomeScreen(
     var locationText by remember { mutableStateOf("No location selected") } // Location street
     var messageText by remember { mutableStateOf("") } // Location city
     var pubsList by remember { mutableStateOf(listOf<Pair<String, String>>()) } // List of pubs (pubId, pubName)
+
+    // State for the drawer
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
 
     // Fetch user name when the screen is loaded
     LaunchedEffect(auth.currentUser?.uid) {
@@ -67,149 +74,240 @@ fun HomeScreen(
         )
     )
 
-    Scaffold(
-        snackbarHost = { SnackbarHost(remember { SnackbarHostState() }) }
-    ) { padding ->
+    // Define the content of the sidebar (navigation drawer)
+    val drawerContent: @Composable () -> Unit = {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(padding)
+                .background(Color.Black)
                 .padding(16.dp)
         ) {
-            // Fixed part (Welcome Message and Update Location Button)
-            userName?.let { name ->
-                Text(
-                    text = "Welcome to Limerick, $name! See below where the best Craic is now.",
-                    style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
-                )
-            }
+            // Spacer para centralizar a palavra "Menu" mais acima
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Text(
+                text = "Menu",
+                style = MaterialTheme.typography.headlineSmall,
+                color = Color.White, // Make the text color white
+                modifier = Modifier.padding(bottom = 16.dp)
+
+            )
+            Spacer(modifier = Modifier.height(24.dp))
 
             Button(
                 onClick = {
-                    if (locationPermissionsState.allPermissionsGranted) {
-                        locationText = "Retrieving location..."
-                        messageText = ""
+                    // Example: Navigate to Profile
+                    coroutineScope.launch { drawerState.close() }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Gray, // Button background color
+                    contentColor = Color.White  // Button text color
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Profile")
+            }
 
-                        // Check if GPS is enabled, then request the new location
-                        checkIfGpsIsEnabled(context) {
-                            getCurrentLocation(context, fusedLocationClient) { location ->
-                                if (location != null) {
-                                    locationText = location.first // Street
-                                    messageText = location.second // City
+            Spacer(modifier = Modifier.height(8.dp))
 
-                                    // Fetch pubs if the city is Limerick
-                                    if (messageText == "Limerick") {
-                                        fetchPubsFromDatabase { pubs ->
-                                            pubsList = pubs
+            // Settings Button
+            Button(
+                onClick = {
+                    // Example: Navigate to Settings
+                    coroutineScope.launch { drawerState.close() }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Gray, // Button background color
+                    contentColor = Color.White  // Button text color
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Settings")
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            // Logout Button
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        drawerState.close()
+                        onLogout() // Log out and navigate to login screen
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Gray, // Button background color
+                    contentColor = Color.White  // Button text color
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Logout")
+            }
+        }
+    }
+
+    // Add the ModalNavigationDrawer
+    ModalNavigationDrawer(
+        drawerContent = drawerContent,
+        drawerState = drawerState,
+        scrimColor = Color.Black.copy(alpha = 0.7f)
+    ) {
+        Scaffold(
+            snackbarHost = { SnackbarHost(remember { SnackbarHostState() }) },
+            topBar = {
+                TopAppBar(
+                    title = { Text("Home") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            coroutineScope.launch { drawerState.open() } // Open the drawer
+                        }) {
+                            Icon(Icons.Filled.Menu, contentDescription = "Menu") // Novo ícone de três tracinhos
+                        }
+                    }
+                )
+            }
+        ) { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .padding(16.dp)
+            ) {
+                // Fixed part (Welcome Message and Update Location Button)
+                userName?.let { name ->
+                    Text(
+                        text = "Welcome to Limerick, $name! See below where the best Craic is now.",
+                        style = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+                    )
+                }
+
+                Button(
+                    onClick = {
+                        if (locationPermissionsState.allPermissionsGranted) {
+                            locationText = "Retrieving location..."
+                            messageText = ""
+
+                            // Check if GPS is enabled, then request the new location
+                            checkIfGpsIsEnabled(context) {
+                                getCurrentLocation(context, fusedLocationClient) { location ->
+                                    if (location != null) {
+                                        locationText = location.first // Street
+                                        messageText = location.second // City
+
+                                        // Fetch pubs if the city is Limerick
+                                        if (messageText == "Limerick") {
+                                            fetchPubsFromDatabase { pubs ->
+                                                pubsList = pubs
+                                            }
+                                        } else {
+                                            pubsList = listOf() // Clear pubs if not in Limerick
+                                            messageText = "No pubs available in your city."
                                         }
                                     } else {
-                                        pubsList = listOf() // Clear pubs if not in Limerick
-                                        messageText = "No pubs available in your city."
+                                        locationText = "Unable to retrieve location"
+                                        messageText = "Unable to retrieve city"
                                     }
-                                } else {
-                                    locationText = "Unable to retrieve location"
-                                    messageText = "Unable to retrieve city"
+                                }
+                            }
+                        } else {
+                            // Request location permissions
+                            locationPermissionsState.launchMultiplePermissionRequest()
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Text("Click to update your location")
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                // Dynamic part (Scrollable list with pubs and logout button)
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize() // Ensure scrollable content takes up the rest of the screen
+                ) {
+                    // Add location details
+                    item {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.padding(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Place,
+                                contentDescription = "Location Icon",
+                                tint = Color.Red, // Icon color
+                                modifier = Modifier.size(20.dp)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(locationText, style = MaterialTheme.typography.bodyLarge)
+                        }
+
+                        if (messageText.isNotEmpty()) {
+                            Text(
+                                text = messageText,
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = Color.Black,
+                                modifier = Modifier.padding(8.dp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
+
+                    // Add pubs list
+                    items(pubsList) { (pubId, pubName) ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            elevation = CardDefaults.cardElevation(4.dp)
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                // Pub Name
+                                Text(
+                                    text = pubName,
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    modifier = Modifier.weight(1f)
+                                )
+
+                                // Floating Action Button for each pub
+                                FloatingActionButton(
+                                    onClick = { onNavigateToPubDetails(pubId) }, // Navigate to details
+                                    modifier = Modifier.size(65.dp),
+                                    containerColor = Color.Red,
+                                    contentColor = Color.White
+                                ) {
+                                    Text("🔥RATE")
                                 }
                             }
                         }
-                    } else {
-                        // Request location permissions
-                        locationPermissionsState.launchMultiplePermissionRequest()
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-            ) {
-                Text("Click to update your location")
-            }
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            // Dynamic part (Scrollable list with pubs and logout button)
-            LazyColumn(
-                modifier = Modifier.fillMaxSize() // Ensure scrollable content takes up the rest of the screen
-            ) {
-                // Add location details
-                item {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.padding(8.dp)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Place,
-                            contentDescription = "Location Icon",
-                            tint = Color.Red, // Icon color
-                            modifier = Modifier.size(20.dp)
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(locationText, style = MaterialTheme.typography.bodyLarge)
                     }
 
-                    if (messageText.isNotEmpty()) {
-                        Text(
-                            text = messageText,
-                            style = MaterialTheme.typography.bodyMedium,
-                            color = Color.Black,
-                            modifier = Modifier.padding(8.dp)
-                        )
-                    }
+                    // Add logout button as the last item in the list
+                    item {
+                        Spacer(modifier = Modifier.height(16.dp))
 
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
-
-                // Add pubs list
-                items(pubsList) { (pubId, pubName) ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        elevation = CardDefaults.cardElevation(4.dp)
-                    ) {
-                        Row(
+                        Button(
+                            onClick = {
+                                coroutineScope.launch {
+                                    FirebaseAuth.getInstance().signOut() // Log out
+                                    onLogout() // Navigate back to the login screen
+                                }
+                            },
+                            colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(16.dp),
-                            verticalAlignment = Alignment.CenterVertically
+                                .padding(8.dp)
                         ) {
-                            // Pub Name
-                            Text(
-                                text = pubName,
-                                style = MaterialTheme.typography.bodyLarge,
-                                modifier = Modifier.weight(1f)
-                            )
-
-                            // Floating Action Button for each pub
-                            FloatingActionButton(
-                                onClick = { onNavigateToPubDetails(pubId) }, // Navigate to details
-                                modifier = Modifier.size(65.dp),
-                                containerColor = Color.Red,
-                                contentColor = Color.White
-                            ) {
-                                Text("🔥RATE")
-                            }
+                            Text("Logout")
                         }
-                    }
-                }
-
-                // Add logout button as the last item in the list
-                item {
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    Button(
-                        onClick = {
-                            coroutineScope.launch {
-                                FirebaseAuth.getInstance().signOut() // Log out
-                                onLogout() // Navigate back to the login screen
-                            }
-                        },
-                        colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                    ) {
-                        Text("Logout")
                     }
                 }
             }
@@ -217,7 +315,7 @@ fun HomeScreen(
     }
 }
 
-// Function to fetch pubs from Firestore
+// Helper function: Fetch pubs from Firestore
 private fun fetchPubsFromDatabase(onResult: (List<Pair<String, String>>) -> Unit) {
     val db = FirebaseFirestore.getInstance()
 
@@ -233,7 +331,7 @@ private fun fetchPubsFromDatabase(onResult: (List<Pair<String, String>>) -> Unit
         }
 }
 
-// Get current location (latitude, longitude)
+// Helper function: Get current location
 @SuppressLint("MissingPermission")
 private fun getCurrentLocation(
     context: Context,
@@ -262,7 +360,7 @@ private fun getCurrentLocation(
     fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
 }
 
-// Check if GPS is enabled
+// Helper function: Check if GPS is enabled
 private fun checkIfGpsIsEnabled(context: Context, onEnabled: () -> Unit) {
     val activity = context as? Activity ?: return
 
@@ -284,7 +382,7 @@ private fun checkIfGpsIsEnabled(context: Context, onEnabled: () -> Unit) {
     }
 }
 
-// Get address from latitude and longitude
+// Helper function: Get address from latitude and longitude
 private fun getAddressFromLocation(context: Context, latitude: Double, longitude: Double): Pair<String, String>? {
     return try {
         val geocoder = Geocoder(context, Locale.getDefault())
--- app/src/main/java/com/example/finalproject_mobdev/ui/theme/Theme.kt
+++ app/src/main/java/com/example/finalproject_mobdev/ui/theme/Theme.kt
@@ -1,44 +1,38 @@
 package com.example.finalproject_mobdev.ui.theme
 
-import android.app.Activity
 import android.os.Build
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material3.*
+import com.example.finalproject_mobdev.R
 
 private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
-
-        /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40,
+    background = Color(0xFFF3E5F5), // Light purple background
+    surface = Color.White
 )
 
 @Composable
 fun Finalproject_MOBDEVTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
-        // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
-        content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -51,8 +45,23 @@ fun Finalproject_MOBDEVTheme(
     }
 
     MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
-    )
+        colorScheme = colorScheme,
+        typography = Typography
+    ) {
+        // Add an image background or a solid background
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            // Use an image as the background
+            Image(
+                painter = painterResource(id = R.drawable.backgroundimage), // Replace with your image file
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+
+            // Pass the screen content
+            content()
+        }
+    }
 }
